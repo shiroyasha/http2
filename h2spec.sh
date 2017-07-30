23476775e576a8281267ea913c2cf499ffd5e04c
Igor Šarčević <igor@renderedtext.com>
2017-07-30 23:39:18 +0200

    Extend sleep duration because desktop computer is slow -.-

--- h2spec.sh
+++ h2spec.sh
@@ -8,7 +8,7 @@ mix deps.get
 mix compile
 mix run --no-halt > ../../log.txt &
 
-sleep 2 # wait for the server to start
+sleep 5 # wait for the server to start
 echo "Waiting for the server to start"
 
 sudo docker run --net="host" -ti summerwind/h2spec --port 8443 -k --verbose $1
