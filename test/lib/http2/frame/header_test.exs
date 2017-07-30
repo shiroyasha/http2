91e813c0508f349e4f1c5b9dc0e3154d41ba814f
Igor Šarčević <igor@renderedtext.com>
2017-07-30 13:40:27 +0200

    Header frame tests

---
+++ test/lib/http2/frame/header_test.exs
@@ -0,0 +1,44 @@
+defmodule Http2.Frame.HeaderTest do
+  use ExUnit.Case
+  doctest Http2
+
+  describe ".decode" do
+
+    setup do
+      {:ok, hpack_table} = HPack.Table.start_link(1000)
+
+      {:ok, hpack_table: hpack_table}
+    end
+
+    test "decoding the flags", %{ hpack_table: hpack_table } do
+      paylaod = <<130, 134, 132, 65, 138, 8, 157, 92, 11, 129, 112, 220, 121, 166, 153>>
+      flags   = encode_flags(1, 0, 0, 1)
+      frame   = %Http2.Frame{ type: :header, flags: flags, len: 16, payload: paylaod }
+      header  = Http2.Frame.Header.decode(frame, hpack_table)
+
+      assert header.end_stream == true
+      assert header.end_headers == false
+      assert header.padded == false
+      assert header.priority == true
+    end
+
+    test "decoding the payload", %{ hpack_table: hpack_table } do
+      paylaod = <<130, 134, 132, 65, 138, 8, 157, 92, 11, 129, 112, 220, 121, 166, 153>>
+      flags   = encode_flags(0, 0, 1, 0)
+      frame   = %Http2.Frame{ type: :header, flags: flags, len: 16, payload: paylaod }
+      header  = Http2.Frame.Header.decode(frame, hpack_table)
+
+      assert header.header_block_fragment == [
+        {":method", "GET"},
+        {":scheme", "http"},
+        {":path", "/"},
+        {":authority", "127.0.0.1:8443"}
+      ]
+    end
+
+  end
+
+  def encode_flags(priority, padded, end_headers, end_stream) do
+    << 0::1, 0::1, priority::1, 0::1, padded::1, end_headers::1, 0::1, end_stream::1>>
+  end
+end
