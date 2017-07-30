00b613c98eaf016e5be1e3938eaa9102821cf7dd
Igor Šarčević <igor@renderedtext.com>
2017-07-30 11:12:59 +0200

    Use older elixir version because 1.5 is not available on CI

--- examples/hello_http2/mix.exs
+++ examples/hello_http2/mix.exs
@@ -4,7 +4,7 @@ defmodule HelloHttp2.Mixfile do
   def project do
     [app: :hello_http2,
      version: "0.0.1",
-     elixir: "~> 1.5",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
