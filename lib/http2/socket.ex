2ae143f1319ed6a53a247d04429ded5d7e4613bc
Igor Šarčević <igor@renderedtext.com>
2017-08-06 19:01:51 +0200

    Remove unused file

--- lib/http2/socket.ex
+++
@@ -1,9 +0,0 @@
-defmodule Http2.Socket do
-  use GenServer
-  require Logger
-
-  def start_link(handler_module, socket) do
-    GenServer.start_link(__MODULE__, {handler_module, socket}, [])
-  end
-
-end
